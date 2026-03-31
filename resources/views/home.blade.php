e453eb49260d651ce0503c786cba16661c03e1fb
b231940106-eit <b231940106@erdenetis.edu.mn>
2026-03-31 18:26:52 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ <link rel="stylesheet" href="/css/style.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Plant Shop</title>
+ </head>
+
+ <header>
+    <nav class="navbar">
+
+    <div class="nav-left">
+        <a href="#">Shop</a>
+        <a href="#">Products</a>
+    </div>
+
+    <div class="nav-center">
+    <img src="/images/logo.png" alt="Plantify">
+    </div>
+
+    <div class="nav-right">
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <i class="fa-regular fa-user"></i>
+    <i class="fa-solid fa-cart-shopping"></i>
+    </div>
+
+    </nav>
+ </header>
+
+ <body>
+
+    <section class="hero">
+
+    <!-- LEFT TEXT -->
+    <div class="hero-left">
+        <h1>Happiness blooms from within</h1>
+
+        <p>
+            Our environment, the world in which we live and work,
+            is a mirror of our attitudes and expectations.
+        </p>
+
+        <div class="hero-buttons">
+            <button class="btn-primary">Shop now</button>
+            <button class="btn-secondary">Explore plants →</button>
+        </div>
+    </div>
+
+    <div class="hero-right">
+        <div class="grid">
+
+            <div class="grid-left">
+                <div class="card small">
+                    <img src="/images/flower1.png">
+                    <div class="overlay">
+                     <h4>New</h4>
+                    </div>
+                </div>
+
+                <div class="card small">
+                    <img src="/images/flower2.png">
+                    <div class="overlay">
+                    <h4>Popular</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card big">
+                <img src="/images/flower3.png">
+                <div class="overlay">
+                    <h3>Anthurium Flower</h3>
+                    <p>The flower of human being. It has meaningful of fact that the plant always grow whatever season and weather.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </section>
+
+    <section class="featured">
+
+    <div class="featured-header">
+        <h2>Featured</h2>
+        <a href="#">view all</a>
+    </div>
+
+    <div class="product-list">
+
+        <!-- PRODUCT -->
+        <div class="product-card">
+            <img src="/images/plant1.png">
+            <h4>Peperomia Ginny</h4>
+            <p>$25</p>
+
+            <button class="buy-btn">Buy</button>
+        </div>
+
+        <div class="product-card">
+            <img src="/images/plant2.png">
+            <h4>Bird’s Nest Fern</h4>
+            <p>$45</p>
+
+            <button class="buy-btn">Buy</button>
+        </div>
+
+        <div class="product-card">
+            <img src="/images/plant3.png">
+            <h4>Large Majesty Palm</h4>
+            <p>$52</p>
+
+            <button class="buy-btn">Buy</button>
+        </div>
+
+        <div class="product-card">
+            <img src="/images/plant4.png">
+            <h4>Pet friendly plants</h4>
+            <p>$45</p>
+
+            <button class="buy-btn">Buy</button>
+        </div>
+
+    </div>
+    </section>
+
+ </body>
+ </html>
